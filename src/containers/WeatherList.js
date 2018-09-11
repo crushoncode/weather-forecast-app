02a94e87448386e0071706c4876c9da16537094d
Serina Ko <serinako08@gmail.com>
2018-09-11 14:50:52 +1000

    map over this.props.weather and get one city per row

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
   render() {
     return (
       <table className="table table-hover">
@@ -13,6 +21,7 @@ class WeatherList extends Component {
             <th>Humidity</th>
           </tr>
         </thead>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
@@ -22,4 +31,4 @@ function mapStateToProps({ weather }) {
   return { weather };
 }
 
-export default connect(mapStatetoProps)(WeatherList);
+export default connect(mapStateToProps)(WeatherList);
