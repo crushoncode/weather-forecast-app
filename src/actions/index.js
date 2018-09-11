c8e9b2e675ed2df66560c9626eb46cee768daa3e
Serina Ko <serinako08@gmail.com>
2018-09-11 12:06:34 +1000

    get url and return promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = '9937e63698d732c84b745c346b6444d3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}, Australia`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
